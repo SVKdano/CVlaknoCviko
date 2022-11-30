3b658194c627263e4a3112ae412991c76a57e560
DanielLieskovsky <danol147@gmail.com>
2022-11-30 10:32:13 +0100

    Oprava nefunkcneho

--- nefunkcne.c
+++ nefunkcne.c
@@ -2,117 +2,113 @@
 #include <pthread.h>
 #include <unistd.h>
 
-typedef struct ThrData {
-    //v zrebcine je niekolko kobyl
-    const int pocetKobyl;
+typedef struct ThrData{
 
-    //ked vyhladne ide sa najest k valovu
+    const int pocetKobyl;
     const int kapacitaNadoby;
 
     int zasoby;
     int indexKobyly;
     int stavNadoby;
 
-    pthread_mutex_t* mutex;
-    pthread_cond_t* cond_spravca;
-    pthread_cond_t* cond_kobyly;
+    pthread_mutex_t * mutex;
+    pthread_cond_t * cond_spravca;
+    pthread_cond_t * cond_kobyly;
 
-}THRDATA;
+}ThrData;
 
-void* spravcaWork(void* sData) {
-    //dostane data v strukture ktoru musim pretypovat
-    THRDATA* data = (THRDATA*)sData;
-
-    while (data->zasoby > 0) {
-        //cakanie na vyprazdnenie valova -> zamknem mutex aby ho nemohol zmenit
+void* spravcaWork(void* sdata){
+    ThrData* data = (ThrData*) sdata;
+    while (data->zasoby > 0){
+        //caka kym bude valov prazdny
         pthread_mutex_lock(data->mutex);
-
-        while (data->stavNadoby > 0) {
-            //pokial nema co robit tak caka
+        while(data->stavNadoby > 0){
             pthread_cond_wait(data->cond_spravca, data->mutex);
         }
-
-        //presiel podmienku kedy je nieco vo valove (je prazdny) -> ide ho doplnat -> musi mat k nemu pristup
         pthread_mutex_unlock(data->mutex);
-        fprintf(stderr, "Spravca ide doplnit nadobu\n");
-        usleep(1000);
-
-        pthread_mutex_lock(data->mutex);
-        //doplnanie nadoby
-        data->stavNadoby = data->kapacitaNadoby;
-        data->zasoby -= data->kapacitaNadoby;
-        pthread_mutex_unlock(data->mutex);
-        fprintf(stderr, "Spravca doplnil nadobu\n");
-
+        fprintf(stderr,"Spravca ide doplnit nadobu\n");
         usleep(500);
-        fprintf(stderr, "Spravca ide zobudit kobyly\n");
-        //zobudzanie kobyl
+        //doplni nadobu
+        fprintf(stderr,"Spravca doplnil nadobu\n");
+        usleep(500);
+        pthread_mutex_lock(data->mutex);
+        data->stavNadoby=data->kapacitaNadoby;
+        data->zasoby-=data->kapacitaNadoby;
+        pthread_mutex_unlock(data->mutex);
+
+        //zavola cond_kobyly, ze valov je plny
+        fprintf(stderr,"Spravca zobudza kobyly\n");
+        usleep(500);
         pthread_cond_broadcast(data->cond_kobyly);
+
     }
 
+
     return NULL;
-};
-
-void* kobylaWork(void* kData) {
-    THRDATA* data = (THRDATA*)kData;
-
+}
+void* kobylaWork(void* sdata){
+    ThrData* data = (ThrData*) sdata;
     pthread_mutex_lock(data->mutex);
-    //vytvaranie kobyl
     int indexKobyly = data->indexKobyly++;
     pthread_mutex_unlock(data->mutex);
-    fprintf(stderr, "Kobyla %d bola vytvorena\n", data->indexKobyly);
+    fprintf(stderr,"Kobyla %d vytvorena\n", indexKobyly);
     usleep(500);
 
-    while (data->zasoby > 0) {
-
+    while (data->zasoby > 0){
+        //ak je nadoba prazdna, tak cakaj
         pthread_mutex_lock(data->mutex);
-        //ak je nadoba prazdna tak cakaj
-        while (data->stavNadoby == 0) {
+        while(data->stavNadoby == 0){
             pthread_cond_wait(data->cond_kobyly, data->mutex);
         }
         pthread_mutex_unlock(data->mutex);
-
-        fprintf(stderr, "Kobyla %d sa najedla\n", data->indexKobyly);
+        //najedz sa
+        fprintf(stderr,"Kobyla %d sa najedla \n", indexKobyly);
         usleep(500);
         pthread_mutex_lock(data->mutex);
         data->stavNadoby--;
         pthread_mutex_unlock(data->mutex);
 
-        //zalarmuje spravcu ze je prazdno
+        // ak je nadoba prazdna, tak zavolaj spravcu
         pthread_cond_signal(data->cond_spravca);
     }
+
+    return NULL;
 }
 
-static int main() {
-    pthread_mutex_t mutex;
-    pthread_cond_t cond_spravca, cond_kobyly;
+
+
+int main() {
+    fprintf(stderr,"Main zacal\n");
+
+    pthread_mutex_t  mutex;
+    pthread_cond_t  cond_spravca, cond_kobyly;
 
     pthread_mutex_init(&mutex, NULL);
     pthread_cond_init(&cond_spravca, NULL);
     pthread_cond_init(&cond_kobyly, NULL);
 
-    THRDATA data = {
+    ThrData data = {
             10,
-            20,
+            19,
             200,
             1,
             20,
             &mutex,
             &cond_spravca,
             &cond_kobyly
-
     };
 
     pthread_t spravca;
     pthread_t kobyly[data.pocetKobyl];
 
-    pthread_create(&spravca, NULL, &spravcaWork, &data);
-    for (int i = 0; i < data.pocetKobyl; i++) {
-        pthread_create(&kobyly[i], NULL, &kobylaWork, &data);
+    pthread_create(&spravca, NULL, &spravcaWork,&data);
+    for (int i = 0; i < data.pocetKobyl; ++i) {
+        pthread_create(&kobyly[i], NULL, &kobylaWork,&data);
     }
 
+
     pthread_join(spravca, NULL);
-    for (int i = 0; i < data.pocetKobyl;i++) {
+    for (int i = 0; i < data.pocetKobyl; ++i) {
         pthread_join(kobyly[i], NULL);
     }
 
