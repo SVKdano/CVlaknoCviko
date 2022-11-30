e30ea33ba97540dec30a1911238cb8fe980ec71d
DanielLieskovsky <danol147@gmail.com>
2022-11-30 09:25:18 +0100

    Cviko

---
+++ cmake-build-debug/CMakeFiles/cvikoUkazkove.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvikoUkazkove.dir/main.c.o"
+  "cvikoUkazkove"
+  "cvikoUkazkove.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cvikoUkazkove.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
